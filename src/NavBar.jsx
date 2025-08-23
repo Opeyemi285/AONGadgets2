f041354c4eb09e78eaefd70e52377dd5ac8f668f
Abdulkabir Abimbola <abimbolaabdulkabir222@gmail.com>
2025-08-23 22:43:56 +0100

    file path

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,4 +1,5 @@
 import './NavBar.css'
+import AonLogo from './assets/aon-logo.png';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Home from './Home.jsx'
 import Cart from './Cart.jsx'
@@ -7,7 +8,7 @@ function Nav() {
         <>
             <nav>
                 <ul className="float-left">
-                    <li><Link to="/"><img src="/assets/aon-logo.png" width="200px" height="50px" alt="AONGadgets Logo" /></Link></li>
+                    <li><Link to="/"><img src={AonLogo} width="200px" height="50px" alt="AONGadgets Logo" /></Link></li>
                     <li className="px-2 desktop"><input type="search" name="search" id="search" placeholder="Search..." /></li>
                     <li className="desktop"><Link to="/login.html"><i className="bi bi-person-circle"></i> Login</Link>/<Link to="/register.html">Register</Link></li>
                     <li className="desktop"><Link to="/favourite.html"><i className="bi bi-heart"></i></Link></li>
