e0285b7994e008401f2a3706f0db640893b95a25
Abdulkabir Abimbola <abimbolaabdulkabir222@gmail.com>
2025-08-25 10:58:37 +0100

    commit

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -10,7 +10,7 @@ function Nav() {
                     <li className="px-2 desktop"><input type="search" name="search" id="search" placeholder="Search..." /></li>
                     <li className="desktop"><Link to="/login.html"><i className="bi bi-person-circle"></i> Login</Link>/<Link to="/register.html">Register</Link></li>
                     <li className="desktop"><Link to="/favourite.html"><i className="bi bi-heart"></i></Link></li>
-                    <li className="desktop"><Link to="/Cart"><i className="bi bi-cart"></i></Link></li>
+                    <li className="desktop"><Link to="/cart"><i className="bi bi-cart"></i></Link></li>
                     <li className="mobile"><Link to="#"><i className="bi bi-list"></i></Link></li>   
                 </ul> 
             </nav>
